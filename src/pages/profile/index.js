4f5ce8c01255834306d99ae8353d250e5e7c1119
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2023-12-30 12:41:31 +0500

    ADDED: Responsive Design Handling

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,19 +5,30 @@ import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import { styled } from '@mui/material/styles'
-import MuiTab from '@mui/material/Tab'
 
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
-import { Grid, Typography, useTheme } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import CourseProgress from '../../@core/custom-components/course-progress'
 
 const ProfileBanner = styled('div')(({ theme }) => ({
   position: 'relative',
   width: '100%',
   height: '130px',
-  [theme.breakpoints.down('md')]: {
-    display: 'none'
+  [theme.breakpoints.down('xs')]: {
+    height: '120px'
+  },
+  [theme.breakpoints.down('sm')]: {
+    height: '120px'
+  },
+  [theme.breakpoints.up('md')]: {
+    height: '120px' // Adjust the height for medium and larger screens
+  },
+  [theme.breakpoints.up('lg')]: {
+    height: '125px' // Adjust the height for large screens
+  },
+  [theme.breakpoints.up('xl')]: {
+    height: '125px' // Adjust the height for large screens
   }
 }))
 
@@ -28,15 +39,36 @@ const ImgStyled = styled('img')(({ theme }) => ({
   borderRadius: '50%',
   position: 'absolute',
   top: 135,
-  right: '556px',
+  right: '50%', // Center the image horizontally
+  transform: 'translateX(50%)', // Center the image horizontally
   border: '4px solid #fff',
-  zIndex: 999
+  zIndex: 999,
+  [theme.breakpoints.up('xs')]: {
+    width: 120, // Adjust width for medium and larger screens
+    height: 120, // Adjust height for medium and larger screens
+    right: '44%',
+    top: 150
+  },
+  [theme.breakpoints.up('md')]: {
+    width: 150, // Adjust width for medium and larger screens
+    height: 150, // Adjust height for medium and larger screens
+    right: '50%'
+  },
+  [theme.breakpoints.up('lg')]: {
+    width: 150, // Adjust width for large screens
+    height: 150, // Adjust height for large screens
+    right: '40%'
+  },
+  [theme.breakpoints.up('xl')]: {
+    width: 150, // Adjust width for large screens
+    height: 150, // Adjust height for large screens
+    right: '44%'
+  }
 }))
 
 const AccountSettings = () => {
   // ** State
   const [imgSrc, setImgSrc] = useState('/images/avatars/3.png')
-  const theme = useTheme()
 
   return (
     <Card style={{ boxShadow: '10px 10px 5px 0px #EBEDEF' }}>
@@ -58,8 +90,8 @@ const AccountSettings = () => {
           Student
         </Typography>
       </Box>
-      <Grid sx={{ marginTop: '50px', padding: '15px 20px' }} container>
-        <Grid item md={6} sx={{ padding: '0px 15px', marginTop: '60px' }}>
+      <Grid sx={{ marginTop: '50px', padding: '15px 20px', width: '100%' }} container>
+        <Grid item xs={12} sm={12} md={6} sx={{ padding: '0px 15px', marginTop: '60px' }}>
           <Typography variant='h5' sx={{ fontWeight: 'bolder' }}>
             Personal Details
           </Typography>
@@ -89,7 +121,7 @@ const AccountSettings = () => {
             </Typography>
           </Box>
         </Grid>
-        <Grid item md={6} sx={{ padding: '0px 15px', marginTop: '60px' }}>
+        <Grid xs={12} sm={12} md={6} sx={{ padding: '0px 15px', marginTop: '60px' }}>
           <Typography variant='h5' sx={{ fontWeight: 'bolder' }}>
             Educational Details
           </Typography>
@@ -103,28 +135,28 @@ const AccountSettings = () => {
             </Typography>
           </Box>
         </Grid>
-        <Grid item md={12} sx={{ padding: '0px 15px', marginTop: '60px' }}>
+        <Grid item xs={12} sm={12} md={12} sx={{ padding: '0px 15px', marginTop: '60px' }}>
           <Typography variant='h5' sx={{ fontWeight: 'bolder' }}>
             Your Courses
           </Typography>
 
           <Grid container>
-            <Grid item md={6}>
+            <Grid item sm={12} md={6}>
               <CourseProgress courseName={"Database and API's Development"} progress={30} />
             </Grid>
           </Grid>
           <Grid container>
-            <Grid item md={6}>
+            <Grid item sm={12} md={6}>
               <CourseProgress courseName={'Frontend Web Development'} progress={40} />
             </Grid>
           </Grid>
           <Grid container>
-            <Grid item md={6}>
+            <Grid item sm={12} md={6}>
               <CourseProgress courseName={'Responsive Design'} progress={70} />
             </Grid>
           </Grid>
           <Grid container>
-            <Grid item md={6}>
+            <Grid item sm={12} md={6}>
               <CourseProgress courseName={'Software Designing'} progress={100} />
             </Grid>
           </Grid>
